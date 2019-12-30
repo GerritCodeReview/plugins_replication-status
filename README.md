629423922b9bf3717d47d7b0e8c3df6fa054ffeb
Fabio Ponciroli <ponch78@gmail.com>
2019-12-30 17:43:34 +0100

    Initial implementation
    Expose project replication-status over REST api
    
    Bug: Issue 14133
    Change-Id: I3320bfa5ed8f54b06387d417d661b42a5ec71909

--- README.md
+++ README.md
@@ -2,3 +2,77 @@
 
 Record and display the repository's replication status without having to dig
 into the Gerrit replication_log
+
+Consumes replication events and updates a cache with the latest replication
+status of specific refs to specific remotes.
+
+The cache information is then exposed via a project's resource REST endpoint:
+
+```bash
+curl -v --user <user> '<gerrit-server>/a/projects/<project-name>/remotes/<remote-url>/replication-status'
+```
+
+* <project-name>: an (url-encoded) project repository
+* <remote-url>: an (url-encoded) remote URL for the replication
+
+For instance, to assess the replication status of the project `some/project` to
+the
+`https://github.com/some/project.git` URL, the following endpoint should be
+called:
+
+```bash
+curl -v --user <user> '<gerrit-server>/a/projects/some%2Fproject/remotes/https%3A%2F%2Fgithub.com%2Fsome%2Fproject.git/replication-status'
+```
+
+A payload, similar to this may be returned:
+
+```
+{
+  "remotes": {
+    "https://github.com/some/project.git": {
+      "status": {
+        "refs/changes/01/1/meta": {
+          "status": "SUCCEEDED",
+          "when": 1626688830
+        },
+        "refs/changes/03/3/meta": {
+          "status": "SUCCEEDED",
+          "when": 1626688854
+        },
+        "refs/changes/03/3/1": {
+          "status": "SUCCEEDED",
+          "when": 1626688854
+        },
+        "refs/changes/02/2/1": {
+          "status": "SUCCEEDED",
+          "when": 1626688844
+        },
+        "refs/changes/02/2/meta": {
+          "status": "SUCCEEDED",
+          "when": 1626688844
+        },
+        "refs/changes/01/1/1": {
+          "status": "SUCCEEDED",
+          "when": 1626688830
+        }
+      }
+    }
+  },
+  "status": "OK",
+  "project": "some/project"
+}
+```
+
+### HTTP status
+
+The endpoint returns different HTTP response code depending on the result:
+
+* 200 OK - All replications are successful to all remotes
+* 404 Not Found - Project was not found
+* 500 Failure - Not all replications are successful to all remotes
+* 403 Forbidden - The user has no permission to query the endpoint. Only
+  Administrators and project owners are allowed.
+
+# TODO
+
+* Does not consume pull-replication events.
---
+++ src/main/java/com/googlesource/gerrit/plugins/replicationstatus/ReplicationStatusProjectRemoteCollection.java
@@ -0,0 +1,58 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replicationstatus;
+
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.extensions.registration.DynamicMap;
+import com.google.gerrit.extensions.restapi.ChildCollection;
+import com.google.gerrit.extensions.restapi.IdString;
+import com.google.gerrit.extensions.restapi.NotImplementedException;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.extensions.restapi.RestView;
+import com.google.gerrit.server.project.ProjectResource;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+@Singleton
+public class ReplicationStatusProjectRemoteCollection
+    implements ChildCollection<ProjectResource, ReplicationStatusProjectRemoteResource> {
+  private final DynamicMap<RestView<ReplicationStatusProjectRemoteResource>> views;
+
+  @Inject
+  ReplicationStatusProjectRemoteCollection(
+      DynamicMap<RestView<ReplicationStatusProjectRemoteResource>> views) {
+    this.views = views;
+  }
+
+  @Override
+  public RestView<ProjectResource> list() throws RestApiException {
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public ReplicationStatusProjectRemoteResource parse(ProjectResource parent, IdString id)
+      throws ResourceNotFoundException, Exception {
+    Project.NameKey projectNameKey = parent.getNameKey();
+    String remoteURL = id.get();
+
+    return new ReplicationStatusProjectRemoteResource(projectNameKey, remoteURL, remoteURL);
+  }
+
+  @Override
+  public DynamicMap<RestView<ReplicationStatusProjectRemoteResource>> views() {
+    return views;
+  }
+}
