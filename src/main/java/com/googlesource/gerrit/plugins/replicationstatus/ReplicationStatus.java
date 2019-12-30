d14a2b05305219b1baeb2717b5a821b0fb0b557b
Fabio Ponciroli <ponch78@gmail.com>
2019-12-30 17:43:34 +0100

    Initial implementation
    Expose project replication-status over REST api
    
    Bug: Issue 14133
    Change-Id: I3320bfa5ed8f54b06387d417d661b42a5ec71909

---
+++ src/main/java/com/googlesource/gerrit/plugins/replicationstatus/ReplicationStatus.java
@@ -0,0 +1,123 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replicationstatus;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.proto.Protos;
+import com.google.gerrit.server.cache.serialize.CacheSerializer;
+import com.googlesource.gerrit.plugins.replicationstatus.proto.Cache;
+
+@AutoValue
+public abstract class ReplicationStatus {
+  static final String CACHE_NAME = "replication_status";
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  static ReplicationStatus create(ReplicationStatusResult status, long when) {
+    return new AutoValue_ReplicationStatus(status, when);
+  }
+
+  public abstract ReplicationStatusResult status();
+
+  public abstract long when();
+
+  public boolean isFailure() {
+    return status().isFailure();
+  }
+
+  @AutoValue
+  public abstract static class Key {
+    static ReplicationStatus.Key create(Project.NameKey projectName, String remote, String ref) {
+      return new AutoValue_ReplicationStatus_Key(projectName, remote, ref);
+    }
+
+    abstract Project.NameKey projectName();
+
+    abstract String remote();
+
+    abstract String ref();
+
+    enum Serializer implements CacheSerializer<ReplicationStatus.Key> {
+      INSTANCE;
+
+      @Override
+      public byte[] serialize(ReplicationStatus.Key object) {
+        return Protos.toByteArray(
+            Cache.ReplicationStatusKeyProto.newBuilder()
+                .setProject(object.projectName().get())
+                .setRemote(object.remote())
+                .setRef(object.ref())
+                .build());
+      }
+
+      @Override
+      public ReplicationStatus.Key deserialize(byte[] in) {
+        Cache.ReplicationStatusKeyProto proto =
+            Protos.parseUnchecked(Cache.ReplicationStatusKeyProto.parser(), in);
+        return ReplicationStatus.Key.create(
+            Project.nameKey(proto.getProject()), proto.getRemote(), proto.getRef());
+      }
+    }
+  }
+
+  enum Serializer implements CacheSerializer<ReplicationStatus> {
+    INSTANCE;
+
+    @Override
+    public byte[] serialize(ReplicationStatus object) {
+      return Protos.toByteArray(
+          Cache.ReplicationStatusProto.newBuilder()
+              .setWhen(object.when())
+              .setStatus(object.status().name())
+              .build());
+    }
+
+    @Override
+    public ReplicationStatus deserialize(byte[] in) {
+      Cache.ReplicationStatusProto proto =
+          Protos.parseUnchecked(Cache.ReplicationStatusProto.parser(), in);
+
+      return ReplicationStatus.create(
+          ReplicationStatus.ReplicationStatusResult.valueOf(proto.getStatus()), proto.getWhen());
+    }
+  }
+
+  enum ReplicationStatusResult {
+    FAILED,
+    NOT_ATTEMPTED,
+    SUCCEEDED,
+    UNKNOWN;
+
+    static ReplicationStatusResult fromString(String result) {
+      switch (result.toLowerCase()) {
+        case "succeeded":
+          return SUCCEEDED;
+        case "not_attempted":
+          return NOT_ATTEMPTED;
+        case "failed":
+          return FAILED;
+        default:
+          logger.atSevere().log(
+              "Could not parse result into a valid replication status: %", result);
+          return UNKNOWN;
+      }
+    }
+
+    public boolean isFailure() {
+      return this == FAILED;
+    }
+  }
+}
---
+++ src/test/java/com/googlesource/gerrit/plugins/replicationstatus/ReplicationStatusIT.java
@@ -0,0 +1,333 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.replicationstatus;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.gerrit.acceptance.testsuite.project.TestProjectUpdate.allow;
+import static com.googlesource.gerrit.plugins.replication.ReplicationState.RefPushResult;
+import static java.util.stream.Collectors.toList;
+import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
+import com.google.gerrit.acceptance.RestResponse;
+import com.google.gerrit.acceptance.TestAccount;
+import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.acceptance.config.GerritConfig;
+import com.google.gerrit.acceptance.testsuite.project.ProjectOperations;
+import com.google.gerrit.common.Nullable;
+import com.google.gerrit.common.data.AccessSection;
+import com.google.gerrit.common.data.Permission;
+import com.google.gerrit.entities.AccountGroup;
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.extensions.api.groups.GroupInput;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.httpd.restapi.RestApiServlet;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.gson.Gson;
+import com.google.inject.Inject;
+import com.googlesource.gerrit.plugins.replication.RefReplicatedEvent;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.storage.file.FileBasedConfig;
+import org.eclipse.jgit.transport.RemoteRefUpdate;
+import org.eclipse.jgit.util.FS;
+import org.junit.Before;
+import org.junit.Test;
+
+@TestPlugin(
+    name = "replication-status",
+    sysModule = "com.googlesource.gerrit.plugins.replicationstatus.Module")
+public class ReplicationStatusIT extends LightweightPluginDaemonTest {
+  private static final String REF_MASTER = Constants.R_HEADS + Constants.MASTER;
+
+  private static final Gson gson = newGson();
+
+  @Inject protected SitePaths sitePaths;
+  @Inject private ProjectOperations projectOperations;
+
+  private EventHandler eventHandler;
+  private ReplicationConfigParser replicationConfigParser;
+  private FileBasedConfig config;
+  protected Path gitPath;
+
+  @Before
+  public void setUp() throws IOException {
+    initConfig();
+    replicationConfigParser = new ReplicationConfigParser(config);
+    eventHandler = plugin.getSysInjector().getInstance(EventHandler.class);
+  }
+
+  @Test
+  @GerritConfig(name = "gerrit.instanceId", value = "testInstanceId-1")
+  public void shouldBeOKSuccessForAdminUsers() throws Exception {
+    String remote = remoteNameFor(project);
+    setReplicationDestination(remote, ImmutableList.of("-mirror"), Optional.empty());
+    RestResponse result = adminRestSession.get(endpoint(project, remote));
+    result.assertOK();
+
+    assertThat(contentWithoutMagicJson(result)).isEqualTo(emptyReplicationStatus(project, remote));
+  }
+
+  @Test
+  @GerritConfig(name = "gerrit.instanceId", value = "testInstanceId-1")
+  public void shouldBeOKSuccessForProjectOwners() throws Exception {
+    makeProjectOwner(user, project);
+    String remote = remoteNameFor(project);
+    setReplicationDestination(remote, ImmutableList.of("-mirror"), Optional.empty());
+
+    RestResponse result = userRestSession.get(endpoint(project, remote));
+    result.assertOK();
+
+    assertThat(contentWithoutMagicJson(result)).isEqualTo(emptyReplicationStatus(project, remote));
+  }
+
+  @Test
+  @GerritConfig(name = "gerrit.instanceId", value = "testInstanceId-1")
+  public void shouldBeForbiddenForNonProjectOwners() throws Exception {
+    String remote = remoteNameFor(project);
+    setReplicationDestination(remote, ImmutableList.of("-mirror"), Optional.empty());
+
+    RestResponse result = userRestSession.get(endpoint(project, remote));
+    result.assertForbidden();
+
+    assertThat(result.getEntityContent()).contains("Administrate Server or Project owner required");
+  }
+
+  @Test
+  @GerritConfig(name = "gerrit.instanceId", value = "testInstanceId-1")
+  public void shouldBeForbiddenForAnonymousUsers() throws Exception {
+    String remote = remoteNameFor(project);
+    setReplicationDestination(remote, ImmutableList.of("-mirror"), Optional.empty());
+
+    RestResponse result = anonymousRestSession.get(endpoint(project, remote));
+    result.assertForbidden();
+
+    assertThat(result.getEntityContent()).contains("Administrate Server or Project owner required");
+  }
+
+  @Test
+  @GerritConfig(name = "gerrit.instanceId", value = "testInstanceId-1")
+  public void shouldNotReportStatusOfReplicationsGeneratedOnDifferentNodes() throws Exception {
+    String remote = remoteNameFor(project);
+    setReplicationDestination(remote, ImmutableList.of("-mirror"), Optional.empty());
+    eventHandler.onEvent(
+        successReplicatedEvent("testInstanceId-2", System.currentTimeMillis(), remote));
+
+    RestResponse result = adminRestSession.get(endpoint(project, remote));
+    result.assertOK();
+
+    assertThat(contentWithoutMagicJson(result)).isEqualTo(emptyReplicationStatus(project, remote));
+  }
+
+  @Test
+  @GerritConfig(name = "gerrit.instanceId", value = "testInstanceId-1")
+  public void shouldReturnSuccessfulProjectReplicationStatus() throws Exception {
+    String remote = remoteNameFor(project);
+    setReplicationDestination(remote, ImmutableList.of("-mirror"), Optional.empty());
+    long eventCreatedOn = System.currentTimeMillis();
+
+    eventHandler.onEvent(successReplicatedEvent("testInstanceId-1", eventCreatedOn, remote));
+    RestResponse result = adminRestSession.get(endpoint(project, remote));
+
+    result.assertOK();
+    assertThat(contentWithoutMagicJson(result))
+        .isEqualTo(successReplicationStatus(remote, project, eventCreatedOn));
+  }
+
+  @Test
+  @GerritConfig(name = "gerrit.instanceId", value = "testInstanceId-1")
+  public void shouldReturnFailedProjectReplicationStatus() throws Exception {
+    String remote = remoteNameFor(project);
+    setReplicationDestination(remote, ImmutableList.of("-mirror"), Optional.empty());
+    long eventCreatedOn = System.currentTimeMillis();
+
+    eventHandler.onEvent(failedReplicatedEvent("testInstanceId-1", eventCreatedOn, remote));
+    RestResponse result = adminRestSession.get(endpoint(project, remote));
+
+    result.assertStatus(SC_INTERNAL_SERVER_ERROR);
+    assertThat(contentWithoutMagicJson(result))
+        .isEqualTo(failedReplicationStatus(remote, project, eventCreatedOn));
+  }
+
+  private String contentWithoutMagicJson(RestResponse response) throws IOException {
+    return response.getEntityContent().substring(RestApiServlet.JSON_MAGIC.length);
+  }
+
+  private AccountGroup.UUID createGroup(String name, TestAccount member) throws RestApiException {
+    GroupInput groupInput = new GroupInput();
+    groupInput.name = name(name);
+    groupInput.members = Collections.singletonList(String.valueOf(member.id().get()));
+    return AccountGroup.uuid(gApi.groups().create(groupInput).get().id);
+  }
+
+  private void makeProjectOwner(TestAccount user, Project.NameKey project) throws RestApiException {
+    projectOperations
+        .project(project)
+        .forUpdate()
+        .add(
+            allow(Permission.OWNER)
+                .ref(AccessSection.ALL)
+                .group(createGroup("projectOwners", user)))
+        .update();
+  }
+
+  private RefReplicatedEvent replicatedEvent(
+      @Nullable String instanceId,
+      long when,
+      String ref,
+      String remote,
+      RefPushResult status,
+      RemoteRefUpdate.Status refStatus) {
+    RefReplicatedEvent replicatedEvent =
+        new RefReplicatedEvent(project.get(), ref, remote, status, refStatus);
+    replicatedEvent.instanceId = instanceId;
+    replicatedEvent.eventCreatedOn = when;
+
+    return replicatedEvent;
+  }
+
+  private RefReplicatedEvent successReplicatedEvent(
+      @Nullable String instanceId, long when, String remote) throws URISyntaxException {
+
+    ArrayList<String> remoteUrls =
+        Lists.newArrayList(replicationConfigParser.remoteUrlsForStanza(remote, project));
+
+    assertThat(remoteUrls.size()).isEqualTo(1);
+
+    return replicatedEvent(
+        instanceId,
+        when,
+        REF_MASTER,
+        remoteUrls.get(0),
+        RefPushResult.SUCCEEDED,
+        RemoteRefUpdate.Status.OK);
+  }
+
+  private RefReplicatedEvent failedReplicatedEvent(
+      @Nullable String instanceId, long when, String remote) throws URISyntaxException {
+    ArrayList<String> remoteUrls =
+        Lists.newArrayList(replicationConfigParser.remoteUrlsForStanza(remote, project));
+
+    assertThat(remoteUrls.size()).isEqualTo(1);
+
+    return replicatedEvent(
+        instanceId,
+        when,
+        REF_MASTER,
+        remoteUrls.get(0),
+        RefPushResult.FAILED,
+        RemoteRefUpdate.Status.REJECTED_NONFASTFORWARD);
+  }
+
+  private static String endpoint(Project.NameKey project, String remote) {
+    return String.format("/projects/%s/remotes/%s/replication-status", project.get(), remote);
+  }
+
+  private String emptyReplicationStatus(Project.NameKey project, String remote)
+      throws URISyntaxException {
+    ArrayList<String> remoteUrls =
+        Lists.newArrayList(replicationConfigParser.remoteUrlsForStanza(remote, project));
+
+    assertThat(remoteUrls.size()).isEqualTo(1);
+
+    return gson.toJson(
+        ProjectReplicationStatus.create(
+            ImmutableMap.of(
+                remoteUrls.get(0), RemoteReplicationStatus.create(Collections.emptyMap())),
+            ProjectReplicationStatus.ProjectReplicationStatusResult.OK,
+            project.get()));
+  }
+
+  private String successReplicationStatus(String remote, Project.NameKey project, long when)
+      throws URISyntaxException {
+    return projectReplicationStatus(
+        remote,
+        project,
+        when,
+        ProjectReplicationStatus.ProjectReplicationStatusResult.OK,
+        ReplicationStatus.ReplicationStatusResult.SUCCEEDED);
+  }
+
+  private String failedReplicationStatus(String remote, Project.NameKey project, long when)
+      throws URISyntaxException {
+    return projectReplicationStatus(
+        remote,
+        project,
+        when,
+        ProjectReplicationStatus.ProjectReplicationStatusResult.FAILED,
+        ReplicationStatus.ReplicationStatusResult.FAILED);
+  }
+
+  private String projectReplicationStatus(
+      String remote,
+      Project.NameKey project,
+      long when,
+      ProjectReplicationStatus.ProjectReplicationStatusResult projectReplicationStatusResult,
+      ReplicationStatus.ReplicationStatusResult replicationStatusResult)
+      throws URISyntaxException {
+
+    ArrayList<String> remoteUrls =
+        Lists.newArrayList(replicationConfigParser.remoteUrlsForStanza(remote, project));
+
+    assertThat(remoteUrls.size()).isEqualTo(1);
+
+    return gson.toJson(
+        ProjectReplicationStatus.create(
+            ImmutableMap.of(
+                remoteUrls.get(0),
+                RemoteReplicationStatus.create(
+                    ImmutableMap.of(
+                        REF_MASTER, ReplicationStatus.create(replicationStatusResult, when)))),
+            projectReplicationStatusResult,
+            project.get()));
+  }
+
+  private void setReplicationDestination(
+      String remoteName, List<String> replicaSuffixes, Optional<String> project)
+      throws IOException {
+
+    List<String> replicaUrls =
+        replicaSuffixes.stream()
+            .map(suffix -> gitPath.resolve("${name}" + suffix + ".git").toString())
+            .collect(toList());
+
+    config.setStringList("remote", remoteName, "url", replicaUrls);
+    project.ifPresent(prj -> config.setString("remote", remoteName, "projects", prj));
+    config.save();
+  }
+
+  private void initConfig() throws IOException {
+    if (config == null) {
+      gitPath = sitePaths.site_path.resolve("git");
+      config =
+          new FileBasedConfig(
+              sitePaths.etc_dir.resolve("replication.config").toFile(), FS.DETECTED);
+      config.save();
+    }
+  }
+
+  private String remoteNameFor(Project.NameKey projectNameKey) {
+    return String.join("-", project.get(), "remote");
+  }
+}
